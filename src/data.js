0548842bc3dd9bfd29dfedb1c432852d67563d38
Lucas Jose Gimenez <lucas.gimenez08@gmail.com>
2021-11-10 17:28:48 -0300

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,58 @@
+//Datos productos
+
+export const arrayProducts = [
+    {
+        id:1,
+        nameProduct:"Beuty Gel de Alore Vera & Ácido Hialuronico",
+        photo:"",
+        shortDescription:"Producto de cuidado en casa, recomendado para el apoyo de pieles secas y con signos de causados por la edad",
+        ingredients: [ "Concentrado de Aloe Vera", "Ácido Hialuronico de Origen Vegetal (1.5%)"],    
+        othersIngredients: ["Agua", "Carbopol", "Glicerina", "Fenoxietanol", "Fragancia Natural del Árbol del Té"],
+        howToUse:"Aplique una capa delgada del gel sore la piel que escoja, dando un suave masaje. También puede usarlo después del baño o por la noche.",
+        notes:"Libre de aceite, libre de Parabenos, sin Hexano y libre de crueldad animal (Vegano)",
+        linkFicha:""
+    },
+    {
+        id:2,
+        nameProduct:"Aloe Vera & Árbol del Té gel marca Prettan",
+        photo:"",
+        shortDescription:"Producto de cuidado en casa, recomendado para el apoyo de pieles secas y con signos de causados por la edad",
+        ingredients:["Concentrado de Aloe Vera", "Árbol del Té (Melaleuca alternifolia"],        othersIngredients:["Agua", "Carbopol", "Glicerina", "Fenoxietanol", "Fragancia Natural del Árbol del Té"],
+        howToUse:"Aplique una capa delgada del gel sore la piel que escoja, dando un suave masaje. También puede usarlo después del baño o por la noche.",
+        notes:"Libre de aceite, libre de Parabenos y libre de crueldad animal (Vegano)",
+        linkFicha:""
+    },
+    {
+        id:3,
+        nameProduct:"Hydro Gel de Lavanda",
+        photo:"",
+        shortDescription:"Producto de cuidado en casa, recomendado para el apoyo de pieles secas y con signos de causados por la edad",
+        ingredients: "Extracto concentrado de Lavanda", 
+        othersIngredients:["Agua", "Carbopol", "Glicerina", "Fenoxietanol", "Fragancia Natural"],
+        howToUse:"Aplique una capa delgada del gel sore la piel que escoja, dando un suave masaje. También puede usarlo después del baño o por la noche.",
+        notes:"Libre de aceite, libre de Parabenos y libre de crueldad animal (Vegano)",
+        linkFicha:""
+    },
+    {
+        id:4,
+        nameProduct:"Gel de Árnica con Manzanilla",
+        photo:"",
+        shortDescription:"Producto de cuidado en casa, recomendado para el apoyo de pieles secas y con signos de causados por la edad",
+        ingredients:["Extracto concentrado de Árnica", "Extracto concentrado de Manzanilla"],
+        othersIngredients:["Agua", "Carbopol", "Glicerina", "Fenoxietanol", "Fragancia Natural"],
+        howToUse:"Aplique una capa delgada del gel sore la piel que escoja, dando un suave masaje. También puede usarlo después del baño o por la noche.",
+        notes:"Libre de aceite, libre de Parabenos y libre de crueldad animal (Vegano)",
+        linkFicha:""
+    },
+    {
+        id:5,
+        nameProduct:"Gel Hidratante de Almendra Dulce",
+        photo:"",
+        shortDescription:"Producto de cuidado en casa, recomendado para el apoyo de pieles secas y con signos de causados por la edad",
+        ingredients: ["Aceite mineral", "Aceite de Durazno", "Fragancia natural de almendra"],
+        othersIngredients:"",
+        howToUse:"Aplique una capa delgada de aceite sore la piel que escoja, dando un suave masaje. También puede usarlo después del baño o por la noche.",
+        notes:"Sin Hexano , sin Parabenos y libre de crueldad animal (Vegano)",
+        linkFicha:""
+    }
+]
