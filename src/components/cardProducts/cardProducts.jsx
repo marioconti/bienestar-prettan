32a380e34dbc394b61341c0a44f3dec3fe9625b5
Mario Conti <marioconti91@gmail.com>
2021-11-10 10:56:28 -0300

    se agregan estilos a la card de products

--- src/components/cardProducts/cardProducts.jsx
+++ src/components/cardProducts/cardProducts.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import './cardProducts.css'
-
+import "./cardProducts.css";
 
 export const CardProducts = () => {
   return (
@@ -9,17 +8,19 @@ export const CardProducts = () => {
         <h3 className="nombre-producto">
           Beuty Gel de Alore Vera & Ácido Hialuronico
         </h3>
-        <img className="imagen-producto" src="" alt="imagen producto" />
+        <a className="more-info" href="">
+          <img className="imagen-producto" src="" alt="imagen producto" />
+        </a>
         <div className="info-descripcion">
           Producto de cuidado en casa, recomendado para el apoyo de pieles secas
           y con signos de causados por la edad
         </div>
       </div>
       <div className="boton">
-        <div className="boton-enlace">Descargar Ficha Técnica</div>
+        <a className="boton-enlace">Descargar Ficha Técnica</a>
       </div>
       <div className="boton">
-        <div className="boton-enlace">Compra</div>
+        <a className="boton-enlace">Comprar</a>
       </div>
     </div>
   );
