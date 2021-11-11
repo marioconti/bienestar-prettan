c0722d7245ae4c420636206aa5ce8860fca85f23
Mario Conti <marioconti91@gmail.com>
2021-11-11 08:48:19 -0300

    se agregan estelos a la card y se agrega carpeta de product

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Header } from "./components/Header/Header";
 
-import { CardProducts } from "./components/cardProducts/cardProducts";
+import { CardProducts } from "./components/CardProducts/cardProducts";
 
 function App() {
   return (
--- src/components/cardProducts/cardProducts.jsx
+++ src/components/cardProducts/cardProducts.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import "./cardProducts.css";
-import aloeVera from '../../assets/images/Aloe-Vera-&-Hyaluronic-Ac.png'
+import aloeVera from "../../assets/images/Aloe-Vera-&-Hyaluronic-Ac.png";
 
 export const CardProducts = () => {
   return (
-    <div className="contenedor-card">
-      <div className="producto">
-        <h3 className="nombre-producto">
+    <div className="card-container">
+      <div className="product">
+        <h3 className="product-name">
           Beuty Gel de Alore Vera & Ácido Hialuronico
         </h3>
         <a className="more-info" href="">
-          <img className="imagen-producto" src={aloeVera} alt="imagen producto" />
+          <img className="image-product" src={aloeVera} alt="imagen producto" />
         </a>
-        <div className="info-descripcion">
+        <div className="info-description">
           Producto de cuidado en casa, recomendado para el apoyo de pieles secas
           y con signos de causados por la edad
         </div>
       </div>
-      <div className="boton">
-        <a className="boton-enlace">Descargar Ficha Técnica</a>
+      <div className="button">
+        <a className="button-enlace">Descargar Ficha Técnica</a>
       </div>
-      <div className="boton">
-        <a className="boton-enlace">Comprar</a>
+      <div className="button">
+        <a className="button-enlace">Comprar</a>
       </div>
     </div>
   );
