32d85d91a0937383912084becb8fbddf7c9ebbe5
Mario Conti <marioconti91@gmail.com>
2021-11-16 22:53:08 -0300

    agrega target _blank a boton compra

--- src/components/cardProducts/cardProducts.jsx
+++ src/components/cardProducts/cardProducts.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import "./CardProducts.css";
 
-export const CardProducts = ({nameProduct, photo, shortDescription, linkFicha, ficha, enlaceAmazon}) => {
+export const CardProducts = ({
+  nameProduct,
+  photo,
+  shortDescription,
+  linkFicha,
+  ficha,
+  enlaceAmazon,
+}) => {
   return (
     <div className="card-container">
       <div className="product">
         <h3 className="product-name">{nameProduct}</h3>
         <a className="more-info" href="">
-          <img
-            className="image-product"
-            src={photo}
-            alt="imagen producto"
-          />
+          <img className="image-product" src={photo} alt="imagen producto" />
         </a>
         <div className="info-description">{shortDescription}</div>
       </div>
-        <a href={linkFicha} download={ficha}>
-          <button className="button">Descargar Ficha Técnica</button>
-        </a>
-        <a href={enlaceAmazon}>
-          <button className='button'>Comprar</button>
-        </a>
-      
+      <a href={linkFicha} download={ficha}>
+        <button className="button">Descargar Ficha Técnica</button>
+      </a>
+      <a href={enlaceAmazon} target="_blank">
+        <button  className="button">
+          Comprar
+        </button>
+      </a>
     </div>
   );
 };
