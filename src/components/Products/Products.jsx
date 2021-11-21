56f88b5bfd4ce8a8d7d59bea123c4a3f8c966033
Mario Conti <marioconti91@gmail.com>
2021-11-21 13:05:22 -0300

    cambio en no se q

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { arrayProducts } from "../../data";
-import { CardProducts } from "../cardProducts/CardProducts";
+import { CardProducts } from "../CardProducts/CardProducts";
 import "./Products.css";
 
 export const ProductList = () => {
