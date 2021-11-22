31d564e5b586697e33d4bfd9dffd848cd9f48fee
Mario Conti <marioconti91@gmail.com>
2021-11-22 20:54:14 -0300

    cambios de estilos. Se agrega boton de vuelta al menu (desktop y responsive)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,42 +1,52 @@
-import React from 'react';
-import mainLogo from '../../assets/logo.png';
-import './Header.css';
-import { Link } from "react-scroll"
+import React from "react";
+import mainLogo from "../../assets/logo.png";
+import "./Header.css";
+import { Link } from "react-scroll";
 
 export const Header = () => {
-    return (
-        <div className='header'>
-            <a href='#'>
-                <img className='mainLogo' src={mainLogo} alt="main logo" />
-            </a>
-            <nav className='nav'>
-                <ul>
-                    <li>home</li>
-                    <li>
-                        <Link 
-                            className="productList" 
-                            to="productList" 
-                            spy={true} 
-                            smooth={true} 
-                            duration={1000}
-                        >
-                        products
-                        </Link>
-                    </li>
-                    <li>
-                        <Link 
-                            className="suscription" 
-                            to="suscription" 
-                            spy={true} 
-                            smooth={true} 
-                            duration={1000}
-                        >
-                        suscription
-                        </Link>
-                    </li>
-                    <li>contact</li>
-                </ul>
-            </nav>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <a href="#">
+        <img className="mainLogo" src={mainLogo} alt="main logo" />
+      </a>
+      <nav className="nav">
+        <ul>
+          <li>home</li>
+          <li>
+            <Link
+              className="productList"
+              to="productList"
+              spy={true}
+              smooth={true}
+              duration={1000}
+            >
+              products
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="suscription"
+              to="suscription"
+              spy={true}
+              smooth={true}
+              duration={1000}
+            >
+              suscription
+            </Link>
+          </li>
+          <li>contact</li>
+          <Link
+            className="link-header"
+            to="header"
+            spy={true}
+            smooth={true}
+            duration={1000}
+          >
+            <i class="fas fa-arrow-circle-up"></i>
+            {/* <i class="fas fa-home"></i> */}
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+};
